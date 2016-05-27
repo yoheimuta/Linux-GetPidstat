a926eacb15c4808ebce1ddeac3a3fa62e026c599
yoheimuta <yoheimuta@gmail.com>
2016-05-27 09:04:30 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Linux::GetPidstat - It's new $module
+
+# SYNOPSIS
+
+    use Linux::GetPidstat;
+
+# DESCRIPTION
+
+Linux::GetPidstat is ...
+
+# LICENSE
+
+Copyright (C) yoheimuta.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+yoheimuta <yoheimuta@gmail.com>
---
+++ lib/Linux/GetPidstat.pm
@@ -0,0 +1,39 @@
+package Linux::GetPidstat;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Linux::GetPidstat - It's new $module
+
+=head1 SYNOPSIS
+
+    use Linux::GetPidstat;
+
+=head1 DESCRIPTION
+
+Linux::GetPidstat is ...
+
+=head1 LICENSE
+
+Copyright (C) yoheimuta.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+yoheimuta E<lt>yoheimuta@gmail.comE<gt>
+
+=cut
+
