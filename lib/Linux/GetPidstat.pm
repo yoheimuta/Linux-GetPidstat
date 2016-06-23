8cbb6f135c61a601469b05d3d766cfd2eed59dec
yoheimuta <yoheimuta@gmail.com>
2016-06-23 19:03:11 +0900

    Added a note about max_child_limit

--- lib/Linux/GetPidstat.pm
+++ lib/Linux/GetPidstat.pm
@@ -177,7 +177,7 @@ Display how to use.
                 --dry_run               Dry run mode. not run the side-effects operation (default:1) (--no-dry_run is also supported)
                 --datetime              Datetime (ex. '2016-06-10 00:00:00') to be recorded
                 --include_child         Flag to be enabled to include child process metrics (default:1) (--no-include_child is also suppoted)
-                --max_child_limit       Number to be used for limiting pidstat multi processes (default:30)
+                --max_child_limit       Number to be used for limiting pidstat multi processes (default:30) (skip this limit if 0 is specified)
                 --mackerel_api_key      An api key to be used for posting to mackerel
                 --mackerel_service_name An mackerel service name
               Requirement Programs: pidstat and pstree commands
